78256dde4e4546df765ffba3caff5ffd65656c0c
yuki-kouno <zgoovw@gmail.com>
2021-02-26 10:59:22 +0900

    feat:firebaseとangularの連携

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireFunctionsModule, REGION } from '@angular/fire/functions';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import 'firebase/firestore';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireFunctionsModule,
+    AngularFireAnalyticsModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
-  providers: [],
+  providers: [
+    { provide: REGION, useValue: 'asia-northeast1' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC8cnvkGPJbFZr7Ru6x1JxGLWmN_EaZTk4',
+    authDomain: 'eventstand-3c145.firebaseapp.com',
+    projectId: 'eventstand-3c145',
+    storageBucket: 'eventstand-3c145.appspot.com',
+    messagingSenderId: '667562475069',
+    appId: '1:667562475069:web:51bc3ca415cdd3ac29f454',
+    measurementId: 'G-BCSGM94VWM',
+  },
 };
 
 /*
