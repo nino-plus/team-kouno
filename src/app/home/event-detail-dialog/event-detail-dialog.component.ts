844f1f476551df02905921bbc6cdd1659586f51d
Yusuke Gunji <inochideutae@gmail.com>
2021-02-27 22:45:57 +0900

    feat(event): add reserveEvent

--- src/app/home/event-detail-dialog/event-detail-dialog.component.ts
+++ src/app/home/event-detail-dialog/event-detail-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Event } from 'src/app/interfaces/event';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-event-detail-dialog',
@@ -11,6 +12,7 @@ import { Event } from 'src/app/interfaces/event';
 export class EventDetailDialogComponent implements OnInit {
   constructor(
     private router: Router,
+    private eventService: EventService,
     @Inject(MAT_DIALOG_DATA)
     public data: {
       event: Event;
@@ -20,7 +22,9 @@ export class EventDetailDialogComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  reserveEvent(event: Event): void {}
+  reserveEvent(event: Event): void {
+    this.eventService.reserveEvent(event, this.data.uid);
+  }
 
   joinChannel(eventId: string, uid: string): void {
     this.router.navigateByUrl(`/event/${eventId}/${uid}`);
---
+++ src/app/interfaces/reserve-uid.ts
@@ -0,0 +1,4 @@
+export interface ReserveUid {
+  uid: string;
+  eventId: string;
+}
