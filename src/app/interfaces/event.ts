ed45d40a620f2585bb6afb7040969692772668c2
Yusuke Gunji <inochideutae@gmail.com>
2021-02-27 14:47:19 +0900

    feat(event.sv): add createEvent, setThumbnailURL

---
+++ src/app/interfaces/event.ts
@@ -0,0 +1,12 @@
+export interface Event {
+  eventId: string;
+  name: string;
+  createdAt: firebase.default.firestore.Timestamp;
+  updatedAt: firebase.default.firestore.Timestamp;
+  startAt: firebase.default.firestore.Timestamp;
+  exitAt: firebase.default.firestore.Timestamp;
+  ownerId: string;
+  thumbnailURL: string;
+  description: string;
+  category: string;
+}
---
+++ src/app/services/event.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Event } from '../interfaces/event';
+import * as firebase from 'firebase';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EventService {
+  constructor(
+    private db: AngularFirestore,
+    private storage: AngularFireStorage
+  ) {}
+
+  async createEvent(
+    event: Omit<Event, 'eventId' | 'thumbnailURL' | 'updatedAt'>,
+    thumbnailURL: string
+  ): Promise<void> {
+    const id = this.db.createId();
+    const image = await this.setThumbnailToStorage(id, thumbnailURL);
+    await this.db.doc<Event>(`events/${id}`).set({
+      ...event,
+      eventId: id,
+      updatedAt: firebase.default.firestore.Timestamp.now(),
+      thumbnailURL: image,
+    });
+  }
+
+  async setThumbnailToStorage(eventId: string, file: string): Promise<string> {
+    const result = await this.storage
+      .ref(`events/${eventId}`)
+      .putString(file, firebase.default.storage.StringFormat.DATA_URL);
+    return result.ref.getDownloadURL();
+  }
+}
