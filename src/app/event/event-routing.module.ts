c9b9d31b296d55fc289ec00a9a02f43576e12bbf
Yusuke Gunji <inochideutae@gmail.com>
2021-02-27 11:52:17 +0900

    feat(event): add routing to event-form

--- src/app/event/event-routing.module.ts
+++ src/app/event/event-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventFormComponent } from './event-form/event-form.component';
+import { EventComponent } from './event/event.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EventComponent,
+  },
+  {
+    path: 'event-form',
+    component: EventFormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class EventRoutingModule { }
+export class EventRoutingModule {}
