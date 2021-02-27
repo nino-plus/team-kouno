172f67bcfe3e80352c1e56c76d8a42360c506e09
yuki-kouno <zgoovw@gmail.com>
2021-02-27 10:53:55 +0900

    feat(event): モジュール、コンポネント作成

---
+++ src/app/event/event-room/event-room.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-room',
+  templateUrl: './event-room.component.html',
+  styleUrls: ['./event-room.component.scss']
+})
+export class EventRoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
