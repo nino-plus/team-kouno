b7b7f907f6a27f3452041ae8f971ec17d4d5d129
Yusuke Gunji <inochideutae@gmail.com>
2021-02-27 11:53:07 +0900

    feat(event-form): add UI

--- src/app/event/event-form/event-form.component.ts
+++ src/app/event/event-form/event-form.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-event-form',
   templateUrl: './event-form.component.html',
-  styleUrls: ['./event-form.component.scss']
+  styleUrls: ['./event-form.component.scss'],
 })
 export class EventFormComponent implements OnInit {
+  readonly NAME_MAX_LENGTH = 20;
+  readonly DESCRIPTION_MAX_LENGTH = 200;
+  readonly PASS_MIN_LENGTH = 6;
+  oldImageUrl = '';
+  imageFile: string;
+  isProcessing: boolean;
+  uid: string;
 
-  constructor() { }
+  form = this.fb.group({
+    name: [
+      '',
+      [Validators.required, Validators.maxLength(this.NAME_MAX_LENGTH)],
+    ],
+    description: [
+      '',
+      [Validators.required, Validators.maxLength(this.DESCRIPTION_MAX_LENGTH)],
+    ],
+    category: ['', [Validators.required]],
+    startTime: ['', [Validators.required]],
+    exitTime: [''],
+  });
 
-  ngOnInit(): void {
+  categoryGroup = [
+    { value: 'スポーツ' },
+    { value: '音楽' },
+    { value: 'フード' },
+    { value: 'ビジネス' },
+    { value: 'プログラミング' },
+    { value: 'アウトドア' },
+    { value: '健康' },
+    { value: '政治経済' },
+    { value: 'モノづくり' },
+    { value: '総合' },
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  onCroppedImage(image: string): void {
+    this.imageFile = image;
   }
 
+  submit(): void {}
 }
--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -7,13 +7,35 @@ import { EventFormComponent } from './event-form/event-form.component';
 import { EventRoomComponent } from './event-room/event-room.component';
 import { ChatComponent } from './chat/chat.component';
 import { StreamComponent } from './stream/stream.component';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { CropperModule } from '@deer-inc/ngx-croppie';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  declarations: [EventComponent, EventFormComponent, EventRoomComponent, ChatComponent, StreamComponent],
+  declarations: [
+    EventComponent,
+    EventFormComponent,
+    EventRoomComponent,
+    ChatComponent,
+    StreamComponent,
+  ],
   imports: [
     CommonModule,
-    EventRoutingModule
-  ]
+    EventRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    CropperModule,
+    MatSelectModule,
+  ],
 })
-export class EventModule { }
+export class EventModule {}
