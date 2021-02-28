e34519c34461188164a577b7068141548fa49c2a
Yusuke Gunji <inochideutae@gmail.com>
2021-02-28 16:28:53 +0900

    feat(home): add info dialog

---
+++ src/app/home/info-dialog/info-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-info-dialog',
+  templateUrl: './info-dialog.component.html',
+  styleUrls: ['./info-dialog.component.scss'],
+})
+export class InfoDialogComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
