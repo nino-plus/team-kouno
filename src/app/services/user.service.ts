c0fd689f0ef336cca4a413bafcfaad835e103372
Yusuke Gunji <inochideutae@gmail.com>
2021-03-04 02:32:02 +0900

    feat(user.sv): add deleteUser

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../interfaces/user';
 
@@ -7,7 +10,14 @@ import { User } from '../interfaces/user';
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private db: AngularFirestore) {}
+  isProcessing: boolean;
+
+  constructor(
+    private db: AngularFirestore,
+    private fnc: AngularFireFunctions,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
   getUserData(uid: string): Observable<User> {
     return this.db.doc<User>(`users/${uid}`).valueChanges();
@@ -17,5 +27,19 @@ export class UserService {
     await this.db.doc<User>(`users/${user.uid}`).update({
       ...user,
     });
+  }
+
+  async deleteUser(user: User): Promise<void> {
+    this.isProcessing = true;
+    const callable = this.fnc.httpsCallable('deleteAfUser');
+    return callable(user.uid)
+      .toPromise()
+      .then(() => {
+        this.snackBar.open('ご利用ありがとうございました');
+        this.router.navigateByUrl('/');
+      })
+      .finally(() => {
+        this.isProcessing = false;
+      });
   }
 }
