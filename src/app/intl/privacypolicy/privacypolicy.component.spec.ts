427429b4904d8235dc3a104233a726058ab4bb4a
Yusuke Gunji <inochideutae@gmail.com>
2021-03-03 20:00:13 +0900

    feat(privacy): add privacypolicy.component

---
+++ src/app/intl/privacypolicy/privacypolicy.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrivacypolicyComponent } from './privacypolicy.component';
+
+describe('PrivacypolicyComponent', () => {
+  let component: PrivacypolicyComponent;
+  let fixture: ComponentFixture<PrivacypolicyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PrivacypolicyComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PrivacypolicyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
