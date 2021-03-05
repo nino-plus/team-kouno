24f985aabd86f977889131ad8c5dc325bd78838d
Yusuke Gunji <inochideutae@gmail.com>
2021-03-06 06:54:15 +0900

    feat(shared): add event-card

---
+++ src/app/shared/event-card/event-card.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { fade } from 'src/app/animations/animations';
+import { Event } from 'src/app/interfaces/event';
+import { EventDetailDialogComponent } from '../event-detail-dialog/event-detail-dialog.component';
+
+@Component({
+  selector: 'app-event-card',
+  templateUrl: './event-card.component.html',
+  styleUrls: ['./event-card.component.scss'],
+  animations: [fade],
+})
+export class EventCardComponent implements OnInit {
+  @Input() event: Event;
+  @Input() uid: string;
+
+  constructor(private dialog: MatDialog) {}
+
+  openDetailDialog(event: Event): void {
+    this.dialog.open(EventDetailDialogComponent, {
+      data: {
+        event,
+        uid: this.uid,
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    console.log(this.event);
+  }
+}
