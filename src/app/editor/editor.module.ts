153ad6fda597e6803be9bec9da6cbd8cf7aee1c5
Yusuke Gunji <inochideutae@gmail.com>
2021-03-07 11:33:57 +0900

    feat(editor): add editor.module

---
+++ src/app/editor/editor.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EditorRoutingModule } from './editor-routing.module';
+import { EditorComponent } from './editor/editor.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { CropperModule } from '@deer-inc/ngx-croppie';
+import { MatSelectModule } from '@angular/material/select';
+import {
+  OwlDateTimeModule,
+  OwlNativeDateTimeModule,
+} from '@danielmoncada/angular-datetime-picker';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@NgModule({
+  declarations: [EditorComponent],
+  imports: [
+    CommonModule,
+    EditorRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    CropperModule,
+    MatSelectModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    MatTooltipModule,
+    MatDividerModule,
+  ],
+})
+export class EditorModule {}
---
+++ src/app/editor/editor/editor.component.ts
@@ -0,0 +1,148 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap, take, tap } from 'rxjs/operators';
+import { Event } from 'src/app/interfaces/event';
+import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { EventService } from 'src/app/services/event.service';
+import * as firebase from 'firebase';
+import { CropperOptions } from '@deer-inc/ngx-croppie';
+
+@Component({
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.scss'],
+})
+export class EditorComponent implements OnInit {
+  readonly NAME_MAX_LENGTH = 20;
+  readonly DESCRIPTION_MAX_LENGTH = 200;
+  readonly PASS_MIN_LENGTH = 6;
+  oldImageUrl = '';
+  imageFile: string;
+  isProcessing: boolean;
+  uid: string;
+  eventId: string;
+  event: Event;
+  event$: Observable<Event> = this.route.paramMap.pipe(
+    tap((params) => console.log(params.get('eventId'))),
+    switchMap((params) => {
+      this.eventId = params.get('eventId');
+      return this.eventService.getEvent(this.eventId);
+    })
+  );
+
+  form = this.fb.group({
+    name: [
+      '',
+      [Validators.required, Validators.maxLength(this.NAME_MAX_LENGTH)],
+    ],
+    description: [
+      '',
+      [Validators.required, Validators.maxLength(this.DESCRIPTION_MAX_LENGTH)],
+    ],
+    category: ['', [Validators.required]],
+    startAt: ['', [Validators.required]],
+    exitAt: [''],
+  });
+
+  categoryGroup = [
+    { value: 'スポーツ' },
+    { value: 'ミュージック' },
+    { value: 'フード' },
+    { value: 'ビジネス' },
+    { value: 'プログラミング' },
+    { value: 'ゲーム' },
+    { value: 'アニメ' },
+    { value: '映画' },
+    { value: '漫画' },
+    { value: '政治経済' },
+    { value: 'ジェンダー' },
+    { value: '地域' },
+    { value: 'オールラウンド' },
+  ];
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private eventService: EventService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.user$.pipe(take(1)).subscribe((user: User) => {
+      this.uid = user.uid;
+    });
+    this.event$.subscribe((event) => {
+      this.event = event;
+
+      if (this.event) {
+        this.oldImageUrl = event.thumbnailURL;
+        console.log(event.thumbnailURL);
+
+        this.form.patchValue({
+          name: event.name,
+          description: event.description,
+          category: event.category,
+          ownerId: event.ownerId,
+          startAt: event.startAt.toDate(),
+          exitAt: event.exitAt.toDate(),
+        });
+      }
+    });
+  }
+
+  onCroppedImage(image: string): void {
+    this.imageFile = image;
+  }
+
+  async submit(): Promise<void> {
+    this.isProcessing = true;
+    const formData = this.form.value;
+    console.log(formData);
+    const eventData = {
+      name: formData.name,
+      description: formData.description,
+      category: formData.category,
+      ownerId: this.uid,
+      participantCount: this.event ? this.event.participantCount : 0,
+      reserveUserCount: this.event ? this.event.reserveUserCount : 0,
+      startAt: formData.startAt,
+      exitAt: formData.exitAt,
+      createdAt: firebase.default.firestore.Timestamp.now(),
+    };
+
+    if (!this.event) {
+      if (this.imageFile !== undefined) {
+        await this.eventService
+          .createEvent(eventData, this.imageFile)
+          .finally(() => (this.isProcessing = false));
+      } else {
+        const defaultImage = 'assets/images/default-image.jpg';
+        await this.eventService
+          .createEvent(eventData, defaultImage)
+          .finally(() => (this.isProcessing = false));
+      }
+    } else {
+      if (this.imageFile !== undefined) {
+        await this.eventService
+          .updateEvent(this.eventId, eventData, this.imageFile)
+          .finally(() => (this.isProcessing = false));
+      } else {
+        const defaultImage = 'assets/images/default-image.jpg';
+        await this.eventService
+          .updateEvent(this.eventId, eventData, defaultImage)
+          .finally(() => (this.isProcessing = false));
+      }
+    }
+  }
+
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any): void {
+    if (this.form.dirty) {
+      $event.preventDefault();
+      $event.returnValue = '作業中の内容が失われますがよろしいですか？';
+    }
+  }
+}
