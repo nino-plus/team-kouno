5c249e77bccdc32d8996fdf1cf1a002b00fd03d0
Yusuke Gunji <inochideutae@gmail.com>
2021-03-21 22:23:56 +0900

    feat(search): add sp style

--- src/app/search/search/search.component.ts
+++ src/app/search/search/search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SearchIndex } from 'functions/node_modules/algoliasearch';
 import { Event } from 'src/app/interfaces/event';
+import { SearchService } from 'src/app/services/search.service';
 
 @Component({
   selector: 'app-search',
@@ -8,8 +12,29 @@ import { Event } from 'src/app/interfaces/event';
 })
 export class SearchComponent implements OnInit {
   events: Event[];
+  searchControl: FormControl = new FormControl();
+  index: SearchIndex;
+  searchOptions = [];
+  result: {
+    nbHits: number;
+    hits: any[];
+  };
 
-  constructor() {}
+  constructor(private router: Router, private SearchService: SearchService) {}
 
   ngOnInit(): void {}
+
+  setSearchQuery(value: string): void {
+    this.searchControl.setValue(value, {
+      emitEvent: false,
+    });
+    console.log(value);
+  }
+
+  routeSearch(searchQuery: string): void {
+    this.router.navigate(['/search'], {
+      queryParamsHandling: 'merge',
+      queryParams: { searchQuery },
+    });
+  }
 }
