e31d3e3e090063a19ae17655b3ff917660423357
yuki-kouno <zgoovw@gmail.com>
2021-03-31 00:31:40 +0900

    ログインUI追加

---
+++ src/app/auto-open-login-menu/auto-open-login-menu.component.ts
@@ -0,0 +1,27 @@
+import { trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auto-open-login-menu',
+  templateUrl: './auto-open-login-menu.component.html',
+  styleUrls: ['./auto-open-login-menu.component.scss'],
+})
+export class AutoOpenLoginMenuComponent implements OnInit {
+  timedOutCloser;
+
+  constructor() {}
+  ngOnInit(): void {}
+
+  mouseEnter(trigger) {
+    if (this.timedOutCloser) {
+      clearTimeout(this.timedOutCloser);
+    }
+    trigger.openMenu();
+  }
+
+  mouseLeave(trigger) {
+    this.timedOutCloser = setTimeout(() => {
+      trigger.closeMenu();
+    }, 50);
+  }
+}
