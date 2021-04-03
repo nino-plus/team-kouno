832adb2c9d7c1c59ba2f70a4e776ca36c3f69a36
Yusuke Gunji <inochideutae@gmail.com>
2021-04-03 20:45:32 +0900

    fix: fix firebase-service-worker

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.3.1/firebase-messaging.js');
 
 firebase.initializeApp({
   apiKey: 'AIzaSyC8cnvkGPJbFZr7Ru6x1JxGLWmN_EaZTk4',
@@ -9,15 +9,28 @@ firebase.initializeApp({
   messagingSenderId: '667562475069',
   appId: '1:667562475069:web:51bc3ca415cdd3ac29f454',
   measurementId: 'G-BCSGM94VWM',
+  databaseURL: 'https://eventstand-3c145-default-rtdb.firebaseio.com',
 });
 
 const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function(payload) {
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: payload.notification.icon
-    };
-    return self.registration.showNotification(notificationTitle, notificationOptions);
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activating.');
 });
