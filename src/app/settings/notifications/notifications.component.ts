40cb361903046ecd203d7313d691d3d743cae4fe
Yusuke Gunji <inochideutae@gmail.com>
2021-04-03 20:50:58 +0900

    feat(notification): add requestPremission

--- src/app/settings/notifications/notifications.component.ts
+++ src/app/settings/notifications/notifications.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { MessagingService } from 'src/app/services/messaging.service';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss']
+  styleUrls: ['./notifications.component.scss'],
 })
 export class NotificationsComponent implements OnInit {
+  user$: Observable<any> = this.authService.user$;
+  uid: string;
 
-  constructor() { }
+  constructor(
+    private messagingService: MessagingService,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  requestPermission(uid: string) {
+    this.messagingService.requestPermission(uid);
   }
-
 }
