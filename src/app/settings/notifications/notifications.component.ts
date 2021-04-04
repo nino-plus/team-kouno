5e84360b64a91b26152fd03291e6f9a5af83dcb6
Yusuke Gunji <inochideutae@gmail.com>
2021-04-04 23:05:01 +0900

    feat(notifications): add rejectToken

--- src/app/settings/notifications/notifications.component.ts
+++ src/app/settings/notifications/notifications.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Token } from 'src/app/interfaces/token';
 import { AuthService } from 'src/app/services/auth.service';
 import { MessagingService } from 'src/app/services/messaging.service';
 
@@ -11,15 +13,24 @@ import { MessagingService } from 'src/app/services/messaging.service';
 export class NotificationsComponent implements OnInit {
   user$: Observable<any> = this.authService.user$;
   uid: string;
+  tokens$: Observable<Token[]>;
 
   constructor(
     private messagingService: MessagingService,
     private authService: AuthService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.user$.pipe(take(1)).subscribe((user) => {
+      this.tokens$ = this.messagingService.getTokens(user.uid);
+    });
+  }
 
   requestPermission(uid: string) {
     this.messagingService.requestPermission(uid);
+  }
+
+  rejectPermission(uid: string) {
+    this.messagingService.deleteToken(uid);
   }
 }
