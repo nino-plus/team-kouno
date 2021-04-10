b385998efc2706834db96f7b62a6c552c020bf78
hide-a1 <hide210g@gmail.com>
2021-04-10 14:15:26 +0900

    feat(confirm): 話しかける前にダイアログをはさむ

---
+++ src/app/shared/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.scss'],
+})
+export class ConfirmDialogComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      text: string;
+    }
+  ) {}
+
+  ngOnInit(): void {}
+}
