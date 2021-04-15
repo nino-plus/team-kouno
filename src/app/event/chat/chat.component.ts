3c35bca46bbb9b1a1cd35830f043f7b6957a7abf
Yusuke Gunji <inochideutae@gmail.com>
2021-04-15 23:53:37 +0900

    feat(chat): add custom tool-bar

--- src/app/event/chat/chat.component.ts
+++ src/app/event/chat/chat.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { Chat, ChatWithUser } from 'src/app/interfaces/chat';
+import { ChatWithUser } from 'src/app/interfaces/chat';
 import { AuthService } from 'src/app/services/auth.service';
 import { ChatService } from 'src/app/services/chat.service';
+import 'quill-emoji/dist/quill-emoji.js';
 
 @Component({
   selector: 'app-chat',
@@ -15,11 +16,17 @@ export class ChatComponent implements OnInit {
   uid = this.authService.uid;
   form = new FormControl();
   chats$: Observable<ChatWithUser[]>;
+  isShow: boolean;
 
   constructor(
     private chatService: ChatService,
     private authService: AuthService
   ) {}
+
+  toggleToolBar() {
+    this.isShow = !this.isShow;
+    console.log(this.isShow);
+  }
 
   createChat(): void {
     if (this.form.value) {
@@ -36,4 +43,21 @@ export class ChatComponent implements OnInit {
   ngOnInit(): void {
     this.chats$ = this.chatService.getChatsWithUser(this.eventId);
   }
+
+  // onSelectionChanged = (event) => {
+  //   if (event.oldRange == null) {
+  //     this.onFocus(event);
+  //   }
+  //   if (event.range == null) {
+  //     this.onBlur(event);
+  //   }
+  // };
+
+  // onFocus(event) {
+  //   event.editor.theme.modules.toolbar.container.style.visibility = 'visible';
+  // }
+
+  // onBlur(event) {
+  //   event.editor.theme.modules.toolbar.container.style.visibility = 'hidden';
+  // }
 }
