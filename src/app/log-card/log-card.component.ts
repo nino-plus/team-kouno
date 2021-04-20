150c46d1499af11516d2f5dcd16b96b81f59ce12
Yusuke Gunji <inochideutae@gmail.com>
2021-04-20 23:59:19 +0900

    feat(app): add log-card

---
+++ src/app/log-card/log-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-card',
+  templateUrl: './log-card.component.html',
+  styleUrls: ['./log-card.component.scss']
+})
+export class LogCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
