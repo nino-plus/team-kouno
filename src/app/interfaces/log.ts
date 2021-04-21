e2ce57d01cb5c848a73add52b05ec67fb1babead
Yusuke Gunji <inochideutae@gmail.com>
2021-04-22 00:18:55 +0900

    feat(log): add LogWithUser

--- src/app/interfaces/log.ts
+++ src/app/interfaces/log.ts
@@ -1,7 +1,13 @@
+import { User } from './user';
+
 export interface Log {
   uid: string;
   logId: string;
   createdAt: firebase.default.firestore.Timestamp;
   eventId?: string;
   type: string;
+}
+
+export interface LogWithUser extends Log {
+  user: User;
 }
