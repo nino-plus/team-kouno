fa7c3e810db11d192aee8000adf951d843a654fd
Yusuke Gunji <inochideutae@gmail.com>
2021-04-22 02:59:59 +0900

    feat(log-card): add reserve log

--- src/app/shared/log-card/log-card.component.ts
+++ src/app/shared/log-card/log-card.component.ts
@@ -1,15 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { fade } from 'src/app/animations/animations';
+import { Event } from 'src/app/interfaces/event';
 import { LogWithUser } from 'src/app/interfaces/log';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-log-card',
   templateUrl: './log-card.component.html',
   styleUrls: ['./log-card.component.scss'],
+  animations: [fade],
 })
 export class LogCardComponent implements OnInit {
   @Input() log: LogWithUser;
+  event$: Observable<Event>;
 
-  constructor() {}
+  constructor(private eventService: EventService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.event$ = this.eventService.getEvent(this.log.logId);
+  }
 }
