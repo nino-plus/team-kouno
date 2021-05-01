4ece180a3a61eee2d1d63724eddfa48bc99be5aa
Yusuke Gunji <inochideutae@gmail.com>
2021-05-01 23:59:13 +0900

    feat(user-list): add callSound play

--- src/app/shared/user-list/user-list.component.ts
+++ src/app/shared/user-list/user-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { shareReplay, skip } from 'rxjs/operators';
 import { fade } from 'src/app/animations/animations';
 import { InviteWithSender } from 'src/app/intefaces/invite';
 import { LogWithUser } from 'src/app/interfaces/log';
@@ -8,7 +8,9 @@ import { User } from 'src/app/interfaces/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { LogService } from 'src/app/services/log.service';
 import { MeetingService } from 'src/app/services/meeting.service';
+import { SoundService } from 'src/app/services/sound.service';
 import { UserService } from 'src/app/services/user.service';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-user-list',
@@ -16,7 +18,10 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./user-list.component.scss'],
   animations: [fade],
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
+  dateNow: firebase.default.firestore.Timestamp = firebase.default.firestore.Timestamp.now();
+
   allUsers$: Observable<User[]> = this.userService.getPublicUsers();
   onlineUsers$: Observable<User[]> = this.userService.getOnlinePublicUsers();
   followings$: Observable<User[]>;
@@ -30,21 +35,31 @@ export class UserListComponent implements OnInit {
     private authService: AuthService,
     private userService: UserService,
     private meetingService: MeetingService,
-    private logService: LogService
+    private logService: LogService,
+    private soundService: SoundService
   ) {}
 
   ngOnInit(): void {
-    this.user$
-      .pipe(take(1))
-      .toPromise()
-      .then((user) => {
-        (this.uid = user?.uid),
-          (this.invites$ = this.meetingService.getInvites(this.uid));
-      });
-    this.user$.subscribe((user) => {
-      this.uid = user?.uid;
+    this.user$.pipe(shareReplay(1)).subscribe((user) => {
+      this.uid = user.uid;
       this.logs$ = this.logService.getLogsWithUser(this.uid);
+
+      this.invites$ = this.meetingService.getInvites(this.uid);
     });
+
+    this.subscription.add(
+      this.invites$.pipe(skip(1), shareReplay(1)).subscribe((invites) => {
+        const lastInvite = invites.shift();
+
+        if (lastInvite.createdAt.toMillis() >= this.dateNow.toMillis()) {
+          this.soundService.callSound.play();
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   changeListSource(type: string): void {
