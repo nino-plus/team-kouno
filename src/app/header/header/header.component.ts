0df0f68b956d84b2b8f460131f03aa21f7919bbc
Yusuke Gunji <inochideutae@gmail.com>
2021-05-04 22:22:43 +0900

    feat(header): adjust for dark mode

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,7 @@ import { AuthService } from 'src/app/services/auth.service';
 export class HeaderComponent implements OnInit {
   snsList = ['google', 'twitter', 'facebook', 'github'];
 
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, public uiService: UiService) {}
 
   ngOnInit(): void {}
 }
--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -7,6 +7,7 @@ export class UiService {
   dialogType: string;
   loading: boolean;
   isOpen = false;
+  scrollHieght: number;
 
   constructor() {}
 
