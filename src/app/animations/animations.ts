7be2fb75001805b6ed0f951c3cace06a1b51d1d4
Yusuke Gunji <inochideutae@gmail.com>
2021-05-05 12:09:29 +0900

    feat(animations): add easeslide

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -1,4 +1,10 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 export const fade = trigger('fade', [
   transition('void => *', [
@@ -6,4 +12,38 @@ export const fade = trigger('fade', [
     animate('.5s ease-out', style({ opacity: 1 })),
   ]),
   transition('* => void', [animate('.5s ease-out', style({ opacity: 0 }))]),
+]);
+
+export const easeSlideForSideNav = trigger('easeSlideForSideNav', [
+  state(
+    'close',
+    style({
+      width: '72px',
+    })
+  ),
+  state(
+    'open',
+    style({
+      width: '256px',
+    })
+  ),
+  transition('close => open', animate('.16s ease-in')),
+  transition('open => close', animate('.16s ease-out')),
+]);
+
+export const easeSlideForContent = trigger('easeSlideForContent', [
+  state(
+    'close',
+    style({
+      'margin-left': '72px',
+    })
+  ),
+  state(
+    'open',
+    style({
+      'margin-left': '256px',
+    })
+  ),
+  transition('close => open', animate('.16s ease-out')),
+  transition('open => close', animate('.16s ease-out')),
 ]);
