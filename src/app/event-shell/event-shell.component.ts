237fa427a8a47d321dbc4753aefd82e47e089bf9
Yusuke Gunji <inochideutae@gmail.com>
2021-05-08 23:56:23 +0900

    fix(event-shell): add animations

--- src/app/event-shell/event-shell.component.ts
+++ src/app/event-shell/event-shell.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  easeSlideForContent,
+  easeSlideForSideNav,
+} from '../animations/animations';
 import { UiService } from '../services/ui.service';
 
 @Component({
   selector: 'app-event-shell',
   templateUrl: './event-shell.component.html',
   styleUrls: ['./event-shell.component.scss'],
+  animations: [easeSlideForContent, easeSlideForSideNav],
 })
 export class EventShellComponent implements OnInit {
   constructor(public uiService: UiService) {}
