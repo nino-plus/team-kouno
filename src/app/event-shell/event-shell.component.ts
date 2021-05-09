c96af62891d4bccbbd0f587f7c37468984397487
Yusuke Gunji <inochideutae@gmail.com>
2021-05-09 23:37:07 +0900

    fix(home): fix loop mode

--- src/app/event-shell/event-shell.component.ts
+++ src/app/event-shell/event-shell.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 import {
   easeSlideForContent,
   easeSlideForSideNav,
@@ -12,7 +14,27 @@ import { UiService } from '../services/ui.service';
   animations: [easeSlideForContent, easeSlideForSideNav],
 })
 export class EventShellComponent implements OnInit {
-  constructor(public uiService: UiService) {}
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(
+    public uiService: UiService,
+    private observer: BreakpointObserver
+  ) {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.observer.observe(['(max-width: 700px)']).subscribe((res) => {
+      setTimeout(() => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
+    });
+  }
 }
