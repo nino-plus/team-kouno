0ecd8cc8902c590ee5b194e67e5cbac1179043da
Yusuke Gunji <inochideutae@gmail.com>
2021-05-23 18:29:12 +0900

    feat(event-list): add navigation-btn

--- src/app/home/event-list/event-list.component.ts
+++ src/app/home/event-list/event-list.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { SearchIndex } from 'functions/node_modules/algoliasearch';
 import { Observable } from 'rxjs';
 import { Event } from 'src/app/interfaces/event';
 import { User } from 'src/app/interfaces/user';
 import { EventService } from 'src/app/services/event.service';
 import { SearchService } from 'src/app/services/search.service';
+import { SwiperOptions } from 'swiper';
+import { SwiperComponent } from 'swiper/angular';
 
 @Component({
   selector: 'app-event-list',
@@ -37,6 +39,89 @@ export class EventListComponent implements OnInit {
 
   onliveEventLists$: Observable<Event[]> = this.eventService.getOnliveEvents();
 
+  @ViewChild('popSlides') popSlides: SwiperComponent;
+
+  onliveConfig: SwiperOptions = {
+    initialSlide: 1,
+    slidesPerView: 5,
+    observer: true,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: '.swiper-nav__btn--liv-next',
+      prevEl: '.swiper-nav__btn--liv-prev',
+    },
+    breakpoints: {
+      350: {
+        slidesPerView: 1,
+      },
+      720: {
+        slidesPerView: 2,
+      },
+      960: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+      1400: {
+        slidesPerView: 5,
+      },
+    },
+  };
+  categoryConfig: SwiperOptions = {
+    slidesPerView: 5,
+    observer: true,
+    spaceBetween: 16,
+    slidesPerGroup: 6,
+    navigation: {
+      nextEl: '.swiper-nav__btn--cat-next',
+      prevEl: '.swiper-nav__btn--cat-prev',
+    },
+    breakpoints: {
+      350: {
+        slidesPerView: 1,
+      },
+      720: {
+        slidesPerView: 2,
+      },
+      960: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+      1400: {
+        slidesPerView: 5,
+      },
+    },
+  };
+  popularConfig: SwiperOptions = {
+    slidesPerView: 5,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: '.swiper-nav__btn--pop-next',
+      prevEl: '.swiper-nav__btn--pop-prev',
+    },
+    breakpoints: {
+      350: {
+        slidesPerView: 1,
+      },
+      720: {
+        slidesPerView: 2,
+      },
+      960: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+      1400: {
+        slidesPerView: 5,
+      },
+    },
+    init: false,
+  };
+
   constructor(
     private eventService: EventService,
     private searchService: SearchService
@@ -52,8 +137,22 @@ export class EventListComponent implements OnInit {
         filters: `exitAt > ${this.eventService.dateNow.toMillis()}`,
       })
       .then((result) => {
-        this.popularEventLists.push(...(result.hits.slice(0, 6) as any[]));
+        this.popularEventLists.push(...(result.hits.slice(0, 12) as any[]));
       });
+  }
+
+  slidePrev(): void {
+    for (let i = 0; i < this.popSlides.swiperRef.slides.length / 2; i++) {
+      this.popSlides.swiperRef.slidePrev();
+    }
+    this.popSlides.swiperRef.slidePrev();
+    console.log(this.popSlides.swiperRef.slides.length);
+  }
+
+  slideNext(): void {
+    for (let i = 0; i < this.popSlides.swiperRef.slides.length / 2; i++) {
+      this.popSlides.swiperRef.slideNext();
+    }
   }
 
   changeListSource(category: string): void {
