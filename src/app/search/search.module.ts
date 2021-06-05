4745eb1e8813d880b48b26224b948d11b280a2b3
Yusuke Gunji <inochideutae@gmail.com>
2021-06-05 15:05:33 +0900

    feat(search): add search module

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -10,9 +10,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SearchHeaderComponent } from './search-header/search-header.component';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-  declarations: [SearchResultComponent, SearchComponent],
+  declarations: [
+    SearchResultComponent,
+    SearchComponent,
+    UserSearchComponent,
+    SearchHeaderComponent,
+  ],
   imports: [
     CommonModule,
     SearchRoutingModule,
@@ -23,6 +32,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatAutocompleteModule,
     FormsModule,
     ReactiveFormsModule,
+    MatSidenavModule,
+    MatMenuModule,
   ],
 })
 export class SearchModule {}
