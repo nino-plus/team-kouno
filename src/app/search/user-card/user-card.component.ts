df5acf1b7d81cf5c1f0e548a63fc3506422bfca0
Yusuke Gunji <inochideutae@gmail.com>
2021-06-11 02:10:44 +0900

    feat(search): add user-card

---
+++ src/app/search/user-card/user-card.component.ts
@@ -0,0 +1,54 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { MeetingService } from 'src/app/services/meeting.service';
+import { MessagingService } from 'src/app/services/messaging.service';
+
+@Component({
+  selector: 'app-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['./user-card.component.scss'],
+})
+export class UserCardComponent implements OnInit {
+  @Input() targetUser: User;
+  tokens: string[] = [];
+  currentUser$: Observable<User> = this.authService.user$;
+
+  constructor(
+    private messagingService: MessagingService,
+    private fns: AngularFireFunctions,
+    private meetingService: MeetingService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  async sendPushMessage(
+    tokens: string[],
+    currentUser: User,
+    roomId: string
+  ): Promise<void> {
+    const callable = this.fns.httpsCallable('sendPushMessage');
+    return callable({
+      tokens,
+      icon: currentUser.avatarURL,
+      name: currentUser.name,
+      roomId,
+    })
+      .toPromise()
+      .then(() => {
+        this.messagingService.receiveMessage();
+      });
+  }
+
+  async enterWaitingRoom(targetUid: string): Promise<void> {
+    const roomId = await this.meetingService.createEmptyRoom(
+      this.authService.uid
+    );
+    this.router.navigate(['waiting', roomId, { targetUid: targetUid }]);
+  }
+}
