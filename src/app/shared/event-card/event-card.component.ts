c549b80936dbd4486116ad140b5b4686328b8eac
yuki-kouno <zgoovw@gmail.com>
2021-07-11 23:17:28 +0900

    イベントカードclick時、詳細画面へ遷移に統一 Fixes #193

--- src/app/shared/event-card/event-card.component.ts
+++ src/app/shared/event-card/event-card.component.ts
@@ -26,11 +26,12 @@ export class EventCardComponent implements OnInit {
   @Input() dbType: string;
   ownerEvents: string[];
   customer: Customer;
+  duringEvent: boolean;
 
   constructor(
+    public eventService: EventService,
     private dialog: MatDialog,
     private router: Router,
-    private eventService: EventService,
     private uiService: UiService,
     private customerService: CustomerService,
     private snackbar: MatSnackBar
@@ -40,6 +41,17 @@ export class EventCardComponent implements OnInit {
     this.customerService
       .getCustomer(this.uid)
       .subscribe((data) => (this.customer = data));
+    if (this.dbType) {
+      this.duringEvent =
+        ((this.event.startAt as unknown) as number) <
+          this.eventService.dateNow.toMillis() - 600000 &&
+        this.event.exitAt >= this.eventService.dateNow;
+    } else {
+      this.duringEvent =
+        this.event.startAt.toMillis() <
+          this.eventService.dateNow.toMillis() - 600000 &&
+        this.event.exitAt >= this.eventService.dateNow;
+    }
   }
 
   openDetailDialog(event: Event, $event): void {
@@ -66,34 +78,31 @@ export class EventCardComponent implements OnInit {
   }
 
   joinChannel(event: Event, $event): void {
-    if (this.dbType === 'algolia') {
-      this.navigateDetail(event, $event);
-    } else {
-      this.eventService
-        .checkPaidUser(event.eventId, this.uid)
-        .pipe(take(1))
-        .subscribe((bool) => {
-          if (bool) {
-            this.join(event, $event);
+    $event.stopPropagation();
+    this.eventService
+      .checkPaidUser(event.eventId, this.uid)
+      .pipe(take(1))
+      .subscribe((bool) => {
+        if (bool) {
+          this.join(event);
+        } else {
+          if (
+            event.headcountLimit <= event.reserveUserCount ||
+            event.headcountLimit <= event.participantCount
+          ) {
+            this.snackbar.open('このイベントには募集定員一杯です');
           } else {
-            if (
-              event.headcountLimit <= event.reserveUserCount ||
-              event.headcountLimit <= event.participantCount
-            ) {
-              this.snackbar.open('このイベントには募集定員一杯です');
+            if (event.price > 0) {
+              this.chackCustomer(event);
             } else {
-              if (event.price > 0) {
-                this.chackCustomer(event, $event);
-              } else {
-                this.join(event, $event);
-              }
+              this.join(event);
             }
           }
-        });
-    }
+        }
+      });
   }
 
-  chackCustomer(event: Event, $event) {
+  chackCustomer(event: Event) {
     this.customerService
       .getCustomer(this.uid)
       .pipe(take(1))
@@ -109,7 +118,7 @@ export class EventCardComponent implements OnInit {
             .afterClosed()
             .subscribe(async (status) => {
               if (status) {
-                this.join(event, $event);
+                this.join(event);
               }
             });
         } else {
@@ -131,21 +140,11 @@ export class EventCardComponent implements OnInit {
       });
   }
 
-  join(event: Event, $event) {
-    if (this.uiService.isLargeScreen()) {
-      if (
-        event.startAt.toMillis() <
-          this.eventService.dateNow.toMillis() - 600000 &&
-        event.exitAt >= this.eventService.dateNow
-      ) {
-        this.router
-          .navigateByUrl(`/event/${event.eventId}/${this.uid}`)
-          .then(
-            () => (this.uiService.sidenavIsOpen = !this.uiService.sidenavIsOpen)
-          );
-      } else {
-        this.navigateDetail(event, $event);
-      }
-    }
+  join(event: Event) {
+    this.router
+      .navigateByUrl(`/event/${event.eventId}/${this.uid}`)
+      .then(
+        () => (this.uiService.sidenavIsOpen = !this.uiService.sidenavIsOpen)
+      );
   }
 }
