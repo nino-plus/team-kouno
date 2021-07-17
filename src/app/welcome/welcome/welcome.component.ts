84eb44d938abe94d303df9db2e51b219e1585b40
Yusuke Gunji <inochideutae@gmail.com>
2021-07-17 14:47:36 +0900

    feat(welcome): add styles

--- src/app/welcome/welcome/welcome.component.ts
+++ src/app/welcome/welcome/welcome.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
+  date = new Date();
+  date2 = Date.now();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.date);
+    console.log(this.date2);
   }
-
 }
