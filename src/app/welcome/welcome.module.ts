06ce01c9e1ca4fb9c8c1496e4cc3eeb331f73e0e
Yusuke Gunji <inochideutae@gmail.com>
2021-07-20 02:50:52 +0900

    feat(welcome): add sp styles

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -5,9 +5,15 @@ import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SignupComponent } from './signup/signup.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FooterModule } from '../footer/footer.module';
 
 @NgModule({
   declarations: [WelcomeComponent, SignupComponent],
-  imports: [CommonModule, WelcomeRoutingModule, MatCheckboxModule],
+  imports: [
+    CommonModule,
+    WelcomeRoutingModule,
+    MatCheckboxModule,
+    FooterModule,
+  ],
 })
 export class WelcomeModule {}
