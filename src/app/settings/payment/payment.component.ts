4bd88e3fe8b4762b62bd3bf8c396687c22814dd5
yuki-kouno <zgoovw@gmail.com>
2021-07-21 14:08:28 +0900

    販売履歴取得 Fixes #201

--- src/app/settings/payment/payment.component.ts
+++ src/app/settings/payment/payment.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnectedAccount } from '@interfaces/connected-account';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { ConnectedAccountService } from 'src/app/services/connected-account.service';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.scss']
+  styleUrls: ['./payment.component.scss'],
 })
 export class PaymentComponent implements OnInit {
+  connectAccount$: Observable<ConnectedAccount> = this.connectAccountService.getConnectedAccount(
+    this.authService.uid
+  );
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private connectAccountService: ConnectedAccountService
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
